5422ccc1c87874b83707af35ac492e4901c8e9f0
David Konsumer <konsumer@jetboystudio.com>
2012-09-19 11:35:43 -0700

    added demo-server & require demo

---
+++ demo/server.js
@@ -0,0 +1,15 @@
+var static = require('node-static');
+
+port = 8080
+
+var file = new(static.Server)(__dirname+'/..');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    });
+}).listen(port);
+
+console.log("Simple demo at http://localhost:"+port+"/demo/simple/");
+console.log("Require demo at http://localhost:"+port+"/demo/simple_with_require/");
+console.log("Hit Ctrl-C to stop demo server.");
