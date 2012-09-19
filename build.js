7b2c244c3e60268bba18cb06010c1d8cf93c22f9
David Konsumer <konsumer@jetboystudio.com>
2012-09-19 14:04:20 -0700

    added build system

---
+++ build.js
@@ -0,0 +1,20 @@
+var uglify = require('uglify-js'),
+  path = require('path');
+  fs = require('fs');
+
+var fname = 'jspos.min.js';
+
+var orig_code = "";
+orig_code += fs.readFileSync(path.join(__dirname, 'lib', 'lexicon.js'), 'utf8');
+orig_code += fs.readFileSync(path.join(__dirname, 'lib', 'lexer.js'), 'utf8');
+orig_code += fs.readFileSync(path.join(__dirname, 'lib', 'POSTagger.js'), 'utf8');
+
+var uglify_options = {
+  strict_semicolons: true,
+  mangle_options: {except: ['$super']},
+  gen_options: {ascii_only: true},
+};
+var minified_code = uglify(orig_code, uglify_options);
+
+fs.writeFileSync(path.join(__dirname, fname), minified_code, 'utf8');
+console.log("Wrote minified file to " + fname);
